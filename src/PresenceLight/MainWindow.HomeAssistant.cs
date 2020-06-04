e52c3b69cc3fee83fd068174d3e20c5f6976d907
Isaac Stith <isaac.stith@microsoft.com>
2020-06-04 16:56:49 -0400

    Adding Tab to MainWindow

---
+++ src/PresenceLight/MainWindow.HomeAssistant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PresenceLight
+{
+
+    public partial class MainWindow : Window
+    {
+        #region Home Assistant
+
+        private async void SaveHASettings_Click(object sender, RoutedEventArgs e)
+        {
+            //btnHue.IsEnabled = false;
+            await SettingsService.SaveSettings(Config);
+            //_hueService = new HueService(Config);
+            CheckHueSettings();
+            lblHueSaved.Visibility = Visibility.Visible;
+            btnHue.IsEnabled = true;
+        }
+        #endregion
+    }
+}
